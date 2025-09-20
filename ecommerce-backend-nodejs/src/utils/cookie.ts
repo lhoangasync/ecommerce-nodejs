3b83b4a966c322bb3fb9190095b5d982649317b4
Le Hoang <vatcvietmy123456@gmail.com>
2025-09-20 21:23:12 +0700

    fix cookie be

--- ecommerce-backend-nodejs/src/utils/cookie.ts
+++ ecommerce-backend-nodejs/src/utils/cookie.ts
@@ -1,22 +1,25 @@
 import { Response } from 'express'
 
 export const setRefreshCookie = (res: Response, refresh_token: string) => {
+  const isProd = process.env.NODE_ENV === 'production'
   res.cookie('refresh_token', refresh_token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
-    maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+    secure: isProd,
+    sameSite: isProd ? 'none' : 'lax',
+    path: '/',
+    maxAge: 7 * 24 * 60 * 60 * 1000
   })
 }
 
 export const clearRefreshCookie = (res: Response) => {
+  const isProd = process.env.NODE_ENV === 'production'
   res.clearCookie('refresh_token', {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict'
+    secure: isProd,
+    sameSite: isProd ? 'none' : 'lax',
+    path: '/'
   })
 }
-
 // export const setAccessCookie = (res: Response, access_token: string) => {
 //   res.cookie('access_token', access_token, {
 //     httpOnly: true,
